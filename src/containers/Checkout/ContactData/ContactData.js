db98f5fc793b0976fdf0cb78e01d3f2c45b965e6
Andrey <andreinew@mail.ru>
2018-07-25 18:39:25 +0000

    working on  making auto creation for orderForm state object in ContactData container

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,31 +8,25 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            pastalCode: ''
-        }, 
+        orderForm: {
+            name: null,
+            street: null,
+            zipCode: null,
+            country: null,
+            email: null,
+            cellNumber: null,
+            deliveryMethod: null
+        },
         loading: false
     }
+    
    
     orderHandler = (event) => {
         this.setState({loading: true});
         const order = {
             ingredients: this.props.ingredients,
-            price: this.props.totalPrice,
-            customer:  {
-                name: 'Indra',
-                adress: {
-                    street: 'Izumruday 1',
-                    zipCode: '134201',
-                    country: 'Panama'  
-                },
-                email: 'test@test.com',
-                cellNumber: '+7 111 111 1111'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.totalPrice
+            
         }
         // in a real app we should recalculate the price on server, because users can manipulate data
 
@@ -50,6 +44,39 @@ class ContactData extends Component {
         // we should use preventDefaul
         event.preventDefault();
         console.log(this.props);
+    }
+    
+    
+    formDataCreator (param_type, elem_type, elem_plecholder, param_value)  {
+        const parameter_object = {
+            elementType: param_type,
+            elementConfig: {
+                type: elem_type,
+                placeholder: elem_plecholder,
+            },
+            value: param_value,
+        };
+        
+        console.log(parameter_object);
+        return parameter_object;
+        
+        
+    }
+    
+    
+    componentDidMount() {
+        console.log('Setup deafault parameter for orderForm', this.state);
+        
+        form_name = {...this.formDataCreator('input','text','Your Name', '')};
+        console.log(form_name);
+        this.setState( {name: form_name,
+            street: this.formDataCreator('input', 'text',' Your stret', ''),
+            zipCode: this.formDataCreator('input', 'text', 'Zip code', ''),
+            country: this.formDataCreator('input', 'text', 'Your Country', ''),
+            email: this.formDataCreator('input', 'email', 'Your Email', ''),
+            cellNumber: this.formDataCreator('input', 'text', 'Your cell phone', ''),
+            deliveryMethod: this.formDataCreator('input', 'text', 'method', '')});
+        console.log(this.state);
     }
     
     render () {
