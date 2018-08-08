27b0df15cec308852c27a4dfeb0b9cd75f4c4c5c
Andrey <andreinew@mail.ru>
2018-08-08 11:07:49 +0000

    286 Installed  redux devtools to the project

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose } from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './store/reducer';
 
 
-const store = createStore(reducer);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware()));
 
 const app = (
     <Provider store={store}>
