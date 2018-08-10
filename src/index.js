33659dde0b56ceb920eaca51070d44db91feabf1
Andrey <andreinew@mail.ru>
2018-08-10 11:03:42 +0000

    289 install redux-thunk for handling asynchronous code

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import {Provider} from 'react-redux';
 import burgerBuilder from './store/reducers/burgerBuilder.js';
 //import order from './store/reducers/order.js';
 
@@ -18,7 +20,7 @@ import burgerBuilder from './store/reducers/burgerBuilder.js';
 
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(burgerBuilder, composeEnhancers(applyMiddleware()));
+const store = createStore(burgerBuilder, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
