43034505b723bbed9a8ff771062f97a9acd6d922
Andrey <andreinew@mail.ru>
2018-08-16 10:29:05 +0000

    316 Added logup logic by usig AUTH_LOGOUT action ctreator

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,22 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+   return dispatch => {
+       setTimeout(() => {
+           dispatch(logout());
+       }, expirationTime * 1000);
+       // multiplying to 1000 turns milliseconds to the real seconds
+   };
+};
+
+
 export const auth = (email, password, isSingup) => {
     return dispatch => {
         dispatch(authStart());
@@ -40,6 +56,7 @@ export const auth = (email, password, isSingup) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn));
             })
             .catch(error => {
                 dispatch(authFail(error.response.data.error));
