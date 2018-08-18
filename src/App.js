a37b69746f8c116af6ba5a0cde8e34cee38e4e02
Andrey <andreinew@mail.ru>
2018-08-18 20:39:38 +0000

    325 Implamented Route proteciton on the FrontEnd by adding routes variable which holds routes depending on whether user authenticated or not

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from '../src/hoc/Layout/Layout';
@@ -16,28 +16,48 @@ class App extends Component {
     this.props.onTryAutoSingup();
   } 
  
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/auth" component={ Auth } />
-            <Route path="/checkout" component={ Checkout }/>
-            <Route path="/orders" component={ Orders } />
-            <Route path="/logout" component= { Logout } />
-            <Route path="/" exact component={ BurgerBuild }/>
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
+    render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={ Auth } />
+                <Route path="/" exact component={ BurgerBuild } />
+                <Redirect to="/" />
+            </Switch>
+        );
+        
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={ Checkout } />
+                    <Route path="/orders" component={ Orders } />
+                    <Route path="/logout" component= { Logout } />
+                    <Route path="/" exact component={ BurgerBuild } />
+                    <Redirect to="/" />
+                </Switch>
+            );
+        }
+      
+        return (
+            <div>
+                <Layout>
+                    { routes }
+                </Layout>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {
-    onTryAutoSingup: () => dispatch(actions.authCheckState())
-  }
+    return {
+        onTryAutoSingup: () => dispatch(actions.authCheckState())
+    }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
 
