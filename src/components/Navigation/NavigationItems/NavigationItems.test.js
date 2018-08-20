61799bc75fcbbb2eb794991dd528f1a3303c60c3
Andrey <andreinew@mail.ru>
2018-08-20 18:46:46 +0000

    344 added test with using contains ezyme method for testing specific node in this case Logout if isAuthenticated id true

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -25,4 +25,9 @@ describe('<NavigationItems />', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
     
+    it('should render Logout element if not authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
+    
 });
