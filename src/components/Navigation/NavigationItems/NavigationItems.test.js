9f1659c5d7cdb18ea973a4057f6caeb78dca3057
Andrey <andreinew@mail.ru>
2018-08-20 18:19:24 +0000

    343 example of multiple tests

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -8,8 +8,21 @@ import NavigationItem from './NavigationItem/NavigationItem';
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
-    it('should render two <NavigationItem /> element if not authenticated', () => {
-        const wrapper = shallow(<NavigationItems />);
-        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    let wrapper; 
+    beforeEach(() => {
+         wrapper = shallow(<NavigationItems />);
     });
+    
+    it('should render two <NavigationItem /> element if not authenticated', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+        //expect method provided by Jest package
+    });
+    
+    it('should render three <NavigationItem /> element if not authenticated', () => {
+        //wrapper = shallow(<NavigationItems isAuthenticated />); one way
+        wrapper.setProps({isAuthenticated: true});
+        // second way by using setProsp method form the enzyme package
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+    
 });
